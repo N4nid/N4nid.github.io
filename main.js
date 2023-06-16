bf384d848df4ac96419f1f4c5e6a0c28b2c84079
N4nid <136599840+N4nid@users.noreply.github.com>
2023-06-16 09:41:10 +0200

    added feature
    can shoot with space

--- main.js
+++ main.js
@@ -3,98 +3,117 @@ document.getElementById("btn").onclick = function fun(){
   bounce();
 }
 
-
-let x = 30;
-let y = 10;
 let speed = 10;
+let xp = speed;
+let yp = speed;
 let xM = speed;
 let yM = xM;
+let pWidth = 20;
+let pHeigth = pWidth;
 let width = 10;
 let heigth = width;
 let doBounce = false;
+let liveTime = 100;
+let canShoot = true;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function bounce(){
-  doBounce = !doBounce;
-  console.log(document.getElementById("cv").clientHeight - speed);
-  while(doBounce){
-    clear();
-    x += xM;
-    y += yM;
-    draw();
+  x = xp;
+  y = yp
+  i = 0;
+  //console.log(document.getElementById("cv").clientHeight - heigth);
+  if(canShoot){
+    canShoot = false;
+    while (i < liveTime) {
     
-    if (x == 0) {
-      xM = xM * -1;
+      if (x == 0) {
+        xM = xM * -1;
+      }
+      else if (x > document.getElementById("cv").width - (width + 5)) {
+        xM = xM * -1;
+      }
+      if (y == 0) {
+        yM = yM * -1;
+      }
+      else if (y > document.getElementById("cv").clientHeight - (heigth + 5)) {
+        yM = yM * -1;
+      }
+    
+      clear(heigth, width, x, y);
+      x += xM;
+      y += yM;
+      draw(heigth, width, x, y);
+    
+      await sleep(50);
+      i++;
     }
-    else if (x > document.getElementById("cv").width - (speed+5)) {
-      xM = xM * -1;
-    }
-    if (y == 0) {
-      yM = yM * -1;
-    }
-    else if (y > document.getElementById("cv").clientHeight - (speed+5)) {
-      yM = yM * -1;
-    }
-      
-    await sleep(50);
   }
+  canShoot = true;
+  clear(heigth,width,x,y);
 }
 
-function draw(){
+function draw(h,w,x,y){
   const canvas = document.getElementById("cv");
   if(canvas.getContext){
     const ctx = canvas.getContext("2d");
-    ctx.fillRect(x,y,heigth,width);
+    ctx.fillRect(x,y,h,w);
     
   }
   
 }
 
-function clear() {
+function clear(h,w,x,y) {
   const canvas = document.getElementById("cv");
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
-    ctx.clearRect(x, y, heigth, width);
+    ctx.clearRect(x, y, h, w);
 
   }
- 
+
 }
 
 document.addEventListener('keydown', function(event) {
   if (event.key == "w") {
-   if(y > 0){
-    clear();
-    y -= speed;
-    draw(); 
+   if(yp > 0){
+    clear(pHeigth,pWidth,xp,yp);
+    yp -= speed;
+    draw(pHeigth,pWidth,xp,yp); 
    }
   }
   if (event.key == "a") {
-    if(x > 0){
-      clear();
-      x -= speed;
-      draw();
+    if(xp > 0){
+      clear(pHeigth,pWidth,xp,yp);
+      xp -= speed;
+      draw(pHeigth,pWidth,xp,yp);
     }
     
   }
   if (event.key == "s") {
     //console.log("yoo");
-    if(y < document.getElementById("cv").clientHeight - (speed+5)){
-      clear();
-      y+=speed;
-      draw();
+    if(yp < document.getElementById("cv").clientHeight - (heigth+5)){
+      clear(pHeigth,pWidth,xp,yp);
+      yp+=speed;
+      draw(pHeigth,pWidth,xp,yp);
     }
     
     
   }
   if (event.key == "d") {
-    if(x < document.getElementById("cv").width - (speed+5)){
-      clear();
-      x += speed;
-      draw();
+    if(xp < document.getElementById("cv").width - (width+5)){
+      clear(pHeigth,pWidth,xp,yp);
+      xp += speed;
+      draw(pHeigth,pWidth,xp,yp);
       
+    }
+    
+  }
+  
+  if(event.key == " "){
+    if(canShoot){
+      bounce();
     }
     
   }
